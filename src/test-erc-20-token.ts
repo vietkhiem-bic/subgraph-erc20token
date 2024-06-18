681c1689c709f5c11f314fcc80f2803d7238da2d
0xted97 <khiemsanh1997@gmail.com>
2024-06-18 15:32:59 +0700

    first commit

--- src/test-erc-20-token.ts
+++ src/test-erc-20-token.ts
@@ -1,10 +1,11 @@
 import { Transfer as TransferEvent } from "../generated/TestERC20Token/TestERC20Token"
-import { Transfer } from "../generated/schema"
+import { AddressStatistic, Transfer } from "../generated/schema"
 
 export function handleTransfer(event: TransferEvent): void {
   let entity = new Transfer(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
+  
   entity.from = event.params.from
   entity.to = event.params.to
   entity.amount = event.params.amount
@@ -13,5 +14,14 @@ export function handleTransfer(event: TransferEvent): void {
   entity.blockTimestamp = event.block.timestamp
   entity.transactionHash = event.transaction.hash
 
+  let addressStatistic = AddressStatistic.load(entity.to);
+  if (addressStatistic == null) {
+    addressStatistic = new AddressStatistic(event.params.to);
+    addressStatistic.address = event.params.to
+    addressStatistic.totalAmount = event.params.amount
+  }
+  addressStatistic.totalAmount = addressStatistic.totalAmount.plus(entity.amount)
+  
+  addressStatistic.save()
   entity.save()
 }
